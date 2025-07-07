2ad1c6ca901ebfdf34a98998befdd62f25366e2f
pickletalk <johnvincent.orcullo11@gmail.com>
2025-07-08 06:36:21 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,8 @@
 local Games = {
     [99567941238278] = "https://raw.githubusercontent.com/gumanba/Scripts/main/InkGame", -- ink game
     [126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", -- grow a garden
-    [109983668079237] = "https://raw.githubusercontent.com/Gregy677/Gunmods-strucid/main/Steal%20a%20brain%20rot"-- steal a brainrot
+    [109983668079237] = "https://raw.githubusercontent.com/Gregy677/Gunmods-strucid/main/Steal%20a%20brain%20rot",-- steal a brainrot
+    [17625359962] = "https://raw.githubusercontent.com/debunked69/solixloader/refs/heads/main/solix%20v2%20new%20loader.lua" --rivals
 }
 
 return Games
