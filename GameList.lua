0569b52cc611263a06b555150075e44a92cbb2fd
pickletalk <johnvincent.orcullo11@gmail.com>
2025-07-08 07:48:41 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
     [99567941238278] = "https://raw.githubusercontent.com/gumanba/Scripts/main/InkGame", -- ink game
-    [126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", -- grow a garden
+    [126884695634066, 126244816328678] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", -- speed hub x
     [109983668079237] = "https://raw.githubusercontent.com/Gregy677/Gunmods-strucid/main/Steal%20a%20brain%20rot",-- steal a brainrot
     [142823291] = "https://rawscripts.net/raw/Universal-Script-Forge-Hub-41461", -- murder mystery 2
     [6516141723] = "https://raw.githubusercontent.com/KINGHUB01/BlackKing-obf/main/Doors%20Blackking%20And%20BobHub", -- doors
