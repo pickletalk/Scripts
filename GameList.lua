dc8e06dadadf4314445008e29209a6ad8f40bc08
pickletalk <johnvincent.orcullo11@gmail.com>
2025-07-08 09:43:29 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,10 +1,31 @@
 local Games = {
-    [99567941238278] = "https://raw.githubusercontent.com/gumanba/Scripts/main/InkGame", -- ink game
-    [126884695634066, 126244816328678, 79546208627805, 3623096087, 87039211657390, 17625359962, 85896571713843, 2753915549, 16732694052, 10449761463, 18901165922] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", -- speed hub x
-    [109983668079237] = "https://raw.githubusercontent.com/Gregy677/Gunmods-strucid/main/Steal%20a%20brain%20rot",-- steal a brainrot
-    [142823291] = "https://rawscripts.net/raw/Universal-Script-Forge-Hub-41461", -- murder mystery 2
-    [6516141723] = "https://raw.githubusercontent.com/KINGHUB01/BlackKing-obf/main/Doors%20Blackking%20And%20BobHub", -- doors
-    [116495829188952] = "https://raw.githubusercontent.com/thantzy/thanhub/refs/heads/main/thanv1" -- dead rails
+    -- Ink game
+    [99567941238278] = "https://raw.githubusercontent.com/gumanba/Scripts/main/InkGame",
+    
+    -- Speed Hub X (multiple games)
+    [126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [126244816328678] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [79546208627805] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [3623096087] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [87039211657390] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [17625359962] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [85896571713843] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [16732694052] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    [18901165922] = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    
+    -- Steal a brainrot
+    [109983668079237] = "https://raw.githubusercontent.com/Gregy677/Gunmods-strucid/main/Steal%20a%20brain%20rot",
+    
+    -- Murder Mystery 2
+    [142823291] = "https://rawscripts.net/raw/Universal-Script-Forge-Hub-41461",
+    
+    -- Doors
+    [6516141723] = "https://raw.githubusercontent.com/KINGHUB01/BlackKing-obf/main/Doors%20Blackking%20And%20BobHub",
+    
+    -- Dead Rails
+    [116495829188952] = "https://raw.githubusercontent.com/thantzy/thanhub/refs/heads/main/thanv1"
 }
 
 return Games
