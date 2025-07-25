48ed8b62250a395b8f69c1f3945e90d8cd37bfb0
pickletalk <johnvincent.orcullo11@gmail.com>
2025-07-25 09:39:40 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -43,7 +43,7 @@ local Games = {
     [70876832253163] = "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/DeadRails",
   
     -- Steal a brainrot
-    [109983668079237] = "https://raw.githubusercontent.com/Youifpg/Steal-a-Brainrot-op/refs/heads/main/Arbixhub-obfuscated.lua",
+    [109983668079237] = "https://raw.githubusercontent.com/Youifpg/Steal-a-Brianrot/refs/heads/main/original_script-obfuscated.lua",
     
     -- Murder Mystery 2
     [142823291] = "https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2",
